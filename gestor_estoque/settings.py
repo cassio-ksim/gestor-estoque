e99c32d36f52b24241b9a71a74b0cf665b07aa93
Cassio Lima <cassio.araujo.lima@gmail.com>
2024-06-19 21:42:07 -0300

    feito a pagina base

--- gestor_estoque/settings.py
+++ gestor_estoque/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5gv$qd5-m60u72n*1x&#bqs*04sa_imlxgrpjy7)-ge24yl&ts'
+SECRET_KEY = (
+    'django-insecure-5gv$qd5-m60u72n*1x&#bqs*04sa_imlxgrpjy7)-ge24yl&ts'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- produtos/models.py
+++ produtos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 from utils.base_models import BaseModel
 
 
@@ -8,12 +9,18 @@ class Local(BaseModel):
         ('D', 'Digital'),
     ]
 
-    name = models.CharField(max_length=50, verbose_name='nome do local armazenado')
-    tipo = models.CharField(max_length=1, choices=TIPOS_DE_LOCAL, verbose_name='tipo do local movimentado')
+    name = models.CharField(
+        max_length=50, verbose_name='nome do local armazenado'
+    )  # noqa: E501
+    tipo = models.CharField(
+        max_length=1,
+        choices=TIPOS_DE_LOCAL,
+        verbose_name='tipo do local movimentado',
+    )  # noqa: E501
 
     class Meta:
         db_table = 'locais'
-        
+
 
 class Movimentacao(BaseModel):
     TIPOS_MOVIMENTACAO = [
@@ -21,15 +28,42 @@ class Movimentacao(BaseModel):
         (-1, 'saida'),
     ]
 
-    produto = models.ForeignKey("produtos.Produto", on_delete=models.CASCADE, verbose_name='produto da movimentação')
-    fornecedor = models.ForeignKey('produtos.Fornecedor', on_delete=models.CASCADE, verbose_name='fornecedor do produto movimentado')
-    quantidade = models.DecimalField(max_digits=10, decimal_places=6, verbose_name='quantidade movimentada')
-    local = models.ForeignKey('produtos.Local', on_delete=models.CASCADE, verbose_name='local da movimentação')
-    tipo = models.IntegerField(choices=TIPOS_MOVIMENTACAO, verbose_name='tipo de movimentação')
+    produto = models.ForeignKey(
+        'produtos.Produto',
+        on_delete=models.CASCADE,
+        verbose_name='produto da movimentação',
+    )  # noqa: E501
+    fornecedor = models.ForeignKey(
+        'produtos.Fornecedor',
+        on_delete=models.CASCADE,
+        verbose_name='fornecedor do produto movimentado',
+    )  # noqa: E501
+    quantidade = models.DecimalField(
+        max_digits=10, decimal_places=6, verbose_name='quantidade movimentada'
+    )  # noqa: E501
+    local = models.ForeignKey(
+        'produtos.Local',
+        on_delete=models.CASCADE,
+        verbose_name='local da movimentação',
+    )  # noqa: E501
+    tipo = models.IntegerField(
+        choices=TIPOS_MOVIMENTACAO, verbose_name='tipo de movimentação'
+    )  # noqa: E501
+
+    preco = models.DecimalField(
+        max_digits=10,
+        decimal_places=6,
+        verbose_name='preço do produto na movimentação',
+    )
+
+    codigo_fabricante = models.CharField(
+        max_length=20,
+        verbose_name='código do fabricante',
+    )
 
     class Meta:
         db_table = 'movimentacoes'
-       
+
 
 class Embalagem(BaseModel):
     nome = models.CharField(max_length=50, verbose_name='nome da embalagem')
@@ -37,27 +71,50 @@ class Embalagem(BaseModel):
 
     class Meta:
         db_table = 'embalagens'
-    
+
 
 class Produto(BaseModel):
     nome = models.CharField(max_length=100, verbose_name='nome do produto')
-    categoria = models.ForeignKey('produtos.Categoria',on_delete=models.CASCADE,verbose_name='categoria do produto')
-    embalagens = models.ManyToManyField('produtos.Embalagem', verbose_name='embalagens do produto')
+    categoria = models.ForeignKey(
+        'produtos.Categoria',
+        on_delete=models.CASCADE,
+        verbose_name='categoria do produto',
+    )  # noqa: E501
+    embalagens = models.ManyToManyField(
+        'produtos.Embalagem', verbose_name='embalagens do produto'
+    )  # noqa: E501
+
+    estoque_minimo = models.FloatField(
+        verbose_name='estoque mínimo do produto',
+    )
+
+    estoque_maximo = models.FloatField(
+        verbose_name='estoque maximo do produto'
+    )
 
     class Meta:
         db_table = 'produtos'
 
+
 class Categoria(BaseModel):
-    nome = models.CharField(max_length=100, verbose_name='nome da categoria', unique=True)
+    nome = models.CharField(
+        max_length=100, verbose_name='nome da categoria', unique=True
+    )  # noqa: E501
 
     class Meta:
         db_table = 'categorias'
 
+
 class Fornecedor(BaseModel):
-    nome_social = models.CharField(max_length=100, verbose_name='nome do social')
-    nome_fantazia = models.CharField(max_length=100,verbose_name='nome fantazia')
-    produtos = models.ManyToManyField('produtos.Produto',verbose_name='produtos do fornecedor')
-    
+    nome_social = models.CharField(
+        max_length=100, verbose_name='nome do social'
+    )  # noqa: E501
+    nome_fantazia = models.CharField(
+        max_length=100, verbose_name='nome fantazia'
+    )  # noqa: E501
+    produtos = models.ManyToManyField(
+        'produtos.Produto', verbose_name='produtos do fornecedor'
+    )  # noqa: E501
 
     class Meta:
         db_table = 'fornecedores'
